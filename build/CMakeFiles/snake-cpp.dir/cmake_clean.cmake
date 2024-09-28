ecfe10ad595160abe36f2d9ebe81d87aca3d7fab
Aggelos Ioannides <ioannides.agg@gmail.com>
2024-09-28 19:53:46 +0300

    Layed out basic CMake configurations and functionality to built upon later.

---
+++ build/CMakeFiles/snake-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake-cpp.dir/main.cpp.obj"
+  "CMakeFiles/snake-cpp.dir/main.cpp.obj.d"
+  "libsnake-cpp.dll.a"
+  "snake-cpp.exe"
+  "snake-cpp.exe.manifest"
+  "snake-cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/snake-cpp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for snake-cpp.
+# This may be replaced when dependencies are built.
